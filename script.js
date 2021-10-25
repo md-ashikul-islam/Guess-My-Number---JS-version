2c8f5ead86a4f68fa7fa70845d8b4070451d304f
Md Ashikul Islam <md.ashikul.islam@g.bracu.ac.bd>
2021-10-25 17:00:24 +0600

    Game logic implemented

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct Number!';
-// console.log(document.querySelector('.message').textContent);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// console.log(document.querySelector('.guess').value);
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Please Insert a Number!';
+    document.querySelector('.message').textContent =
+      'Please Insert a Number! ⚠️';
+  } else if (score >= 1) {
+    if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = 'Too high! 👆';
+      score--;
+      document.querySelector('.score').textContent = score;
+      if (score === 0) {
+        document.querySelector('.message').textContent = 'You lost! ☹️';
+      }
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = 'Too low! 👇';
+      score--;
+      document.querySelector('.score').textContent = score;
+      if (score === 0) {
+        document.querySelector('.message').textContent = 'You lost! ☹️';
+      }
+    }
   }
 });
